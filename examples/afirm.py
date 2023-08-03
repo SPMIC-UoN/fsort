f8b365c65018e22205798374bb659b1f15b9eae1
Martin Craig <martin.craig@nottingham.ac.uk>
2023-08-03 15:48:56 +0100

    Implement more afirm sorting

--- examples/afirm.py
+++ examples/afirm.py
@@ -67,7 +67,7 @@ class B1(Sorter):
             self.filter(echonumber=2)
         else:
             # Philips classic protocol
-            self.scale("scaleslope", inverse=True)
+            self.scale("PhilipsRescaleSlope", inverse=True)
 
         self.select_latest()
         self.save("b1")
@@ -113,13 +113,84 @@ class T1(Sorter):
     def __init__(self):
         Sorter.__init__(self, "t1")
 
+    def _add_std(self):
+        self.add(seriesdescription="molli", imagetype="T1 MAP")
+        if not self.have_files():
+            self.add(seriesdescription="molli", imagetype="T1")
+            self.filter(imagetype="MAP")
+        if not self.have_files():
+            self.add(seriesdescription="molli", imagetype="MIXED")
+    
+    def _filter_std(self):
+        self.remove(nslices=3, reason="3-slice heart scan")
+        self.remove(seriesdescription="500", reason="TD500 scan addition to TD0 scan")
+        self.remove(seriesdescription="DelREc", reason="delayed reconstruction")
+        self.remove(seriesdescription="6.5", reason="6.5mm slice")
+        if self.count(seriesdescription="highres") > 0:
+            self.filter(seriesdescription="highres", reason="Not high-res and we have other data that is")
+        if self.count(multislice=True) > 0:
+            self.filter(multislice=True, reason="Not multi-slice and we have other data that is")
+
+    def run_philips(self):
+        self._add_std()
+        self._filter_std()
+        self.group("affine")
+        self.select_latest()
+
+        if self.count(nvols=1) > 0:
+            nvols, map_vol, conf_vol = 1, 0, 0
+        if self.count(nvols=10) > 0:
+            nvols, map_vol, conf_vol = 10, 1, 3
+        elif self.count(nvols=9) > 0:
+            nvols, map_vol, conf_vol = 9, 0, 2
+        elif self.count(nvols=2) > 0:
+            nvols, map_vol, conf_vol = 2, 0, 1
+        else:
+            nvols, map_vol, conf_vol = "UNKNOWN", 0, 0
+            
+        LOG.info(f" - Found {nvols}-volume data from Philips - taking volume {map_vol} for T1 map, {conf_vol} for confidence")
+        self.save("t1_map", vol=map_vol)
+        self.save("t1_conf", vol=conf_vol)
+
+    def run_ge(self):
+        self._add_std()
+        if not self.have_files():
+            self.add(seriesdescription="molli", imagetype="MDE")
+        self._filter_std()
+        self.group("affine")
+        self.select_latest()
+        if self.count(nvols=1) > 0:
+            self.save("t1_map")
+            self.save("t1_conf")
+        else:
+            # FIXME T1 map reconstruction for AFIRM?
+            self.save("t1_map", vol=0)
+            self.save("t1_conf", vol=2)
+
+class T1w(Sorter):
+    def __init__(self):
+        Sorter.__init__(self, "t1w")
+
     def run(self):
-        pass
+        self.add(seriesdescription="t1w")
+        self.select_latest()
+        self.save("t1w_map")
+
+class T2w(Sorter):
+    def __init__(self):
+        Sorter.__init__(self, "t2w")
+
+    def run(self):
+        self.add(seriesdescription="t2w")
+        self.select_latest()
+        self.save("t2w_map")
 
 SORTERS = [
     B0(),
     B1(),
     MTR(),
     T1(),
+    T1w(),
+    T2w(),
     T2star(),
 ]
