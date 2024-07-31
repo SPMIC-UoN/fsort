a79a4dab2f6a032086143b2e2622c1ff93401fe7
Martin Craig <martin.craig@nottingham.ac.uk>
2024-07-31 09:47:26 +0100

    Revise to make it easier to specify input/output dirs in flexible ways

--- fsort/fsort.py
+++ fsort/fsort.py
@@ -4,12 +4,12 @@ FSORT: Run study-specific FSORT configurations
 import importlib
 import logging
 import os
+from pathlib import Path
 import shutil
 import subprocess
 import sys
 
 from .image_file import ImageFile
-from . import xnat
 
 LOG = logging.getLogger(__name__)
 
@@ -45,21 +45,7 @@ class Fsort:
         finally:
             sys.path.remove(config_dirname)
 
-    def run(self):
-        """
-        Main entry point
-        
-        We have to determine if we are using XNAT as our data source or a local folder. A local folder
-        will contain a single session, whereas XNAT could return multiple sessions
-        """
-        if self._options.xnat_host:
-            xnat_sessions = xnat.get_sessions(self._options)
-            for xnat_session in xnat_sessions:
-                self._run_session(xnat_session.output, xnat_session.dicom)
-        else:
-            self._run_session(self._options.output, self._options.dicom, self._options.nifti)
-
-    def _run_session(self, output, dicom_in=None, niftidirs=None):
+    def run(self, output, dicom_in=None, niftidirs=None):
         """
         Run file sorting on a single subject session
 
@@ -68,11 +54,19 @@ class Fsort:
         metadata for each file. Then we pass the file list to each of the
         Sorter modules in turn to extract and rename the files it needs
         """
-        LOG.info(f"RUNNING session - output in {output}")
         if self._options.output_subfolder:
             output = os.path.join(output, self._options.output_subfolder)
+        self._mkdir(output)
+        logfile = os.path.join(output, "logfile.txt")
+        if os.path.exists(logfile):
+            os.remove(logfile)
+        handler = logging.FileHandler(logfile)
+        handler.setFormatter(logging.Formatter('%(levelname)s: %(message)s'))
+        logging.getLogger().addHandler(handler)
+        LOG.info(f" - Output dir: {output}")
 
         if dicom_in:
+            LOG.info("DICOM->NIFTI conversion")
             if not niftidirs:
                 niftidirs = [os.path.join(output, "nifti")]
             elif len(niftidirs) > 1:
@@ -80,27 +74,28 @@ class Fsort:
 
             niftidir = niftidirs[0]
             if self._options.skip_dcm2niix and os.path.exists(niftidir) and os.listdir(niftidir):
-                LOG.info(" - NIFTI files already found - skipping dcm2niix conversion")
+                LOG.info(f" - NIFTI files already found in {niftidir} - skipping dcm2niix conversion")
             else:
                 self._dcm2niix(dicom_in, niftidir, self._options.dcm2niix_args)
         elif not niftidirs:
             raise RuntimeError("Must specify DICOM or NIFTI input folder")
 
-        LOG.info(f" - NIFTI files in {niftidirs}")
+        LOG.info(f"Scanning NIFTI files in {niftidirs}")
         vendor_files = self._scan_niftis(niftidirs, allow_no_vendor=self._options.allow_no_vendor, allow_dupes=self._options.allow_dupes)
         if not vendor_files:
-            LOG.warn(f"No session files found - cannot process session")
+            LOG.warn(f"No session files found")
         else:
             for vendor, files in vendor_files.items():
                 LOG.info(f" - Vendor: {vendor} ({len(files)} files)")
             
             for sorter in self._config.SORTERS:
                 outdir = os.path.join(output, sorter.name)
-                LOG.info(f"Sorting files for module: {sorter.name} - output in {outdir}")
+                LOG.info(f"FSORT MODULE: {sorter.name.upper()} -> {outdir}")
                 self._mkdir(outdir)
                 for vendor, files in vendor_files.items():
                     sorter.process_files(files, vendor, outdir)
-        LOG.info(f"FSORT DONE session - output in {output}")
+                LOG.info(f"FSORT MODULE: {sorter.name.upper()} DONE")
+        LOG.info(f"FSORT DONE -> {output}")
 
     def _start_logfile(self, output_folder):
         """
@@ -134,30 +129,27 @@ class Fsort:
         """
         Run DCM2NIIX if we are using DICOM input
 
-        We use a method where we first drill down to a list of folders one for
-        each scan. Then we run DCM2NIIX on each of these folders in turn. This
-        is more robust than running DCM2NIIX from the top level as if one scan
-        fails to convert, the others can still go through.
+        We walk the dicomdir tree and run dcm2niix on every folder that contains files
         """
-        while 1:
-            scan_dirs = [os.path.join(dicomdir, d) for d in os.listdir(dicomdir) if os.path.isdir(os.path.join(dicomdir, d))]
-            if len(scan_dirs) != 1:
-                break
-            dicomdir = scan_dirs[0]
-
         self._mkdir(niftidir)
         args = args.split()
         dcm2niix_cmd = ["dcm2niix", "-o", niftidir] + args + ["-d", "10", "-z", "y", "-b", "y"]
-        LOG.info("DICOM->NIFTI conversion")
-        for scan_dir in scan_dirs:
-            cmd = dcm2niix_cmd + [scan_dir]
-            LOG.debug(" ".join(cmd))
-            try:
-                output = subprocess.check_output(cmd)
-                LOG.debug(output)
-            except subprocess.CalledProcessError as exc:
-                LOG.warn(f"dcm2niix failed for {scan_dir} with exit code {exc.returncode}")
-                LOG.warn(exc.output)
+        scandirs = []
+        for root, _dirs, files in os.walk(dicomdir, topdown=False):
+            if files:
+                scandir = Path(os.path.join(dicomdir, root))
+                for scandir_done in scandirs:
+                    if scandir_done in scandir.parents:
+                        continue
+                cmd = dcm2niix_cmd + [str(scandir)]
+                LOG.info(f" - Converting DICOMS in {scandir}")
+                LOG.debug(" ".join(cmd))
+                try:
+                    output = subprocess.check_output(cmd)
+                    LOG.debug(output)
+                except subprocess.CalledProcessError as exc:
+                    LOG.warn(f"dcm2niix failed for {scandir} with exit code {exc.returncode}")
+                    LOG.warn(exc.output)
 
     def _scan_niftis(self, niftidirs, allow_no_vendor=False, allow_dupes=False):
         """
--- fsort/main.py
+++ fsort/main.py
@@ -8,6 +8,7 @@ import logging
 
 from ._version import __version__
 from .fsort import Fsort
+from . import xnat
 
 LOG = logging.getLogger(__name__)
 
@@ -33,23 +34,24 @@ def main():
     """
     parser = argparse.ArgumentParser(f'File pre-sorter v{__version__}', add_help=True)
     parser.add_argument('--config', help='Path to Python configuration file', required=True)
-    parser.add_argument('--dicom', help='Path to DICOM input')
-    parser.add_argument('--nifti', help='Path to NIFTI input', nargs="*")
-    parser.add_argument("--skip-dcm2niix", help="Skip DCM2NIIX conversion where NIFTI dir already exists and contains files", action="store_true", default=False)
+    parser.add_argument('--dicom', help='Path to DICOM input. If --input or --subjects-file-has-dir this is a relative path')
+    parser.add_argument('--nifti', help='Path to NIFTI input. If --input or --subjects-file-has-dir these are relative paths', nargs="*")
+    parser.add_argument("--input", "--subjects-dir", help="Input base directory. Subject ID will be appended if not already present")
+    parser.add_argument("--subjects-file", help="File containing subject IDs and optionally tab separated input directories")
+    parser.add_argument("--subjects-file-has-dirs", action="store_true", default=False, help="File containing subject IDs also has input directories")
+    parser.add_argument("--subject", "--xnat-subject", help="Subject ID")
+    parser.add_argument("--subject-idx", "--xnat-subject-idx", type=int, help="Specify subject by zero-based index number into --subjects-file or --input subdirs")
     parser.add_argument("--xnat-host", help="XNAT host url")
     parser.add_argument("--xnat-project", help="Project ID")
-    parser.add_argument("--subjects-dir", help="Directory containing subject subfolders")
-    parser.add_argument("--subjects-file", help="File containing subject IDs")
-    parser.add_argument("--subject", "--xnat-subject", help="Subject ID")
-    parser.add_argument("--subject-idx", "--xnat-subject-idx", type=int, help="Specify subject by index number (starting at zero)")
     parser.add_argument("--xnat-session", help="Session ID")
     parser.add_argument("--xnat-session-idx", type=int, help="Session index (starting at zero)")
     parser.add_argument('--xnat-dicom-output', help='Path to store initial DICOM downloaded files. If not specified will use dicom')
     parser.add_argument("--xnat-user", help="XNAT username. If not supplied, environment variable XNAT_USER will be used or interactive prompt provided")
     parser.add_argument("--xnat-skip-downloaded", help="Skip subjects where DICOM dir already exists", action="store_true", default=False)
-    parser.add_argument('--output', help='Path to output folder - if --subjects-dir is given, the subject ID will be appended')
+    parser.add_argument('--output', help='Path to output folder. If not specified, --input will be used. Subject ID will be appended if not already present')
     parser.add_argument('--output-subfolder', help='Subfolder relative to output')
     parser.add_argument('--nifti-output', help='Path (relative to output) to store initial NIFTI converted files. If not specified, will use nifti')
+    parser.add_argument("--skip-dcm2niix", help="Skip DCM2NIIX conversion where NIFTI dir already exists and contains files", action="store_true", default=False)
     parser.add_argument('--dcm2niix-args', help='DCM2NIIX arguments for DICOM->NIFTI conversion', default="-m n -f %d_%q")
     parser.add_argument('--allow-no-vendor', action="store_true", default=False, help='If specified, process files even when no vendor can be identified')
     parser.add_argument('--allow-dupes', action="store_true", default=False, help='If specified, process files even when another file was found with same image contents')
@@ -70,31 +72,55 @@ def main():
         parser.error("Only one of NIFTI, DICOM or XNAT input can be provided")
     if options.subject and options.subject_idx:
         parser.error("Only one of SUBJECT and SUBJECT_IDX can be provided")
-    if options.subjects_dir and not options.subject and options.subject_idx is None:
-        parser.error("SUBJECTS_DIR/SUBJECTS_FILE provided, but neither SUBJECT or SUBJECT_IDX was given")
+    if (options.input or options.subjects_file) and not (options.subject or options.subject_idx is not None):
+        parser.error("INPUT/SUBJECTS_FILE provided, but neither SUBJECT or SUBJECT_IDX was given")
+    if options.subjects_file_has_dirs and not options.subjects_file:
+        parser.error("No subjects file given, but --subjects-file-has-dirs specified")
     if options.subject_idx is not None and options.subject_idx < 0:
         parser.error("SUBJECT_IDX must be >= 0")
 
-    if (options.dicom or options.nifti) and options.subjects_dir and (options.subject or options.subject_idx is not None):
+    fsort = Fsort(options)
+    if options.xnat_host:
+        xnat_sessions = xnat.get_sessions(options)
+        for xnat_session in xnat_sessions:
+            fsort.run(xnat_session.output, xnat_session.dicom)
+    else:
         if options.subject_idx is not None:
+            # Identify subject from index number
             if options.subjects_file:
                 with open(options.subjects_file, "r") as f:
                     subjects = [l.strip() for l in f.readlines() if l.strip()]
-            else:
+            elif options.subjects_dir:
                 subjects = [d for d in os.listdir(options.subjects_dir) if os.path.isdir(os.path.join(options.subjects_dir, d))]
+                subjects = sorted(subjects)
+            else:
+                parser.error("--subject-idx given but neither --subjects-file nor --input was specified")
             if options.subject_idx >= len(subjects):
-                parser.error(f"Invalid subject IDX {options.subject_idx} - only {len(subjects)} subjects found")
-            options.subject = sorted(subjects)[options.subject_idx]
-            LOG.info(f"Subject ID: {options.subject}")
+                parser.error(f"Invalid --subject-idx {options.subject_idx} - only {len(subjects)} subjects found")
 
-        if options.dicom:
-            options.dicom = os.path.join(options.subjects_dir, options.subject, options.dicom)
-        if options.nifti:
-            options.nifti = [os.path.join(options.subjects_dir, options.subject, n) for n in options.nifti]
-        if options.output:
-            options.output = os.path.join(options.subjects_dir, options.subject, options.output)
+            options.subject = subjects[options.subject_idx]
+            if options.subjects_file_has_dirs:
+                options.subject, options.input = options.subject.split("\t", 1)
+            LOG.info(f" - Subject ID: {options.subject}")
 
-    Fsort(options).run()
+        if options.input and not options.subjects_file_has_dirs:
+            # Input base directory may need subject appending
+            if options.subject and not options.input.strip().endswith(options.subject):
+                options.input = os.path.join(options.input, options.subject)
+
+        if options.input or options.subjects_file_has_dirs:
+            # DICOM/NIFTI paths are relative if we have a subject input directory from elsewhere
+            if options.dicom:
+                options.dicom = os.path.join(options.input, options.dicom)
+            if options.nifti:
+                options.nifti = [os.path.join(options.input, n) for n in options.nifti]
+
+        if not options.output:
+            options.output = options.input
+        elif options.subject and not options.output.strip().endswith(options.subject):
+            options.output = os.path.join(options.output, options.subject)
+
+        fsort.run(options.output, options.dicom, options.nifti)
 
 if __name__ == "__main__":
     main()
