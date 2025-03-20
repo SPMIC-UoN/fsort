566c7bcf05a6605ae321b2eb51d857004f4f17b9
Martin Craig <martin.craig@nottingham.ac.uk>
2025-03-20 15:38:17 +0000

    Move dixon sorter into generic and tweaks to molli and T2/T1w

--- pipelines/afirm.py
+++ pipelines/afirm.py
@@ -4,68 +4,9 @@ FSORT config for afirm
 import logging
 
 from fsort.sorter import Sorter
-from fsort.sorters import T2, T11MolliMapOrRaw, T1MolliRaw, T1SERaw, SeriesDesc, B0TwoEchos, B1, MTR, T2star
+from fsort.sorters import *
 
 LOG = logging.getLogger(__name__)
-
-class MDixon(Sorter):
-    def __init__(self, name, seriesdesc_inc="mdixon", seriesdesc_exc=None):
-        Sorter.__init__(self, name)
-        self._inc = seriesdesc_inc
-        self._exc = seriesdesc_exc
-
-    def run(self):
-        self.add(seriesdescription=self._inc, nvols=3)
-        if self._exc:
-            self.remove(seriesdescription=self._exc)
-        if self.have_files():
-            LOG.info(" - Found 3-volume mDIXON data")
-            self.select_latest()
-            self.save("water", vol=0)
-            self.save("fat", vol=1)
-            self.save("fat_fraction", vol=2)
-            self.clear_selection()
-            self.add(seriesdescription=self._inc, imagetype="T2", nvols=1)
-            if self._exc:
-                self.remove(seriesdescription=self._exc)
-            if self.have_files():
-                LOG.info(" - Found T2* map")
-                self.select_latest()
-                self.save("t2star", vol=0)
-            else:
-                LOG.warn("Could not find single-volume T2* map to go with fat/water/ff")
-        else:
-            # Separate files, need to do filename matching unfortunately
-            LOG.info(" - Did not find 3-volume mDIXON data, doing filename matching")
-            for suffix in ["RM_e1", "RM"]:
-                self.add(seriesdescription=self._inc, imagetype="T2", nvols=1, fname=f"{suffix}a")
-                if self._exc:
-                    self.remove(seriesdescription=self._exc)
-                if self.have_files():
-                    self.select_latest()
-                    self.save("water")
-                    self.clear_selection()
-                    self.add(seriesdescription=self._inc, imagetype="T2", nvols=1, fname=f"{suffix}b")
-                    if self._exc:
-                        self.remove(seriesdescription=self._exc)
-                    self.select_latest()
-                    self.save("fat")
-                    self.clear_selection()
-                    self.add(seriesdescription=self._inc, imagetype="T2", nvols=1, fname=f"{suffix}_real")
-                    if self._exc:
-                        self.remove(seriesdescription=self._exc)
-                    self.select_latest()
-                    self.save("fat_fraction")
-                    self.clear_selection()
-                    self.add(seriesdescription=self._inc, imagetype="T2", nvols=1, fname=f"{suffix}")
-                    if self._exc:
-                        self.remove(seriesdescription=self._exc)
-                    self.remove(fname=f"{suffix}a")            
-                    self.remove(fname=f"{suffix}b")            
-                    self.remove(fname=f"{suffix}_real")      
-                    self.select_latest()      
-                    self.save("t2star")
-                    break
 
 class MolliAx(Sorter):
     def __init__(self):
@@ -112,13 +53,13 @@ class MolliCor(Sorter):
 SORTERS = [
     T1SERaw(),
     T1MolliRaw(),
-    T11MolliMapOrRaw(name="t1_molli"),
+    T11MolliMapOrRaw(),
     T2(),
     B0TwoEchos(add_removes=["aorta"]),
     B1(seriesdescription="B1"),
     MTR(),
-    SeriesDesc("t1w"),
-    SeriesDesc("t2w"),
+    SeriesDesc("t1w", seriesdesc=["cor_t1w", "t1w"]),
+    SeriesDesc("t2w", seriesdesc=["cor_t2w", "t2w"]),
     T2star(),
     MolliAx(),
     MolliCor(),
