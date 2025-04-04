a8967e3c8a676aa37c3b970167c5318e11a38930
Martin Craig <martin.craig@nottingham.ac.uk>
2025-04-04 14:07:29 +0100

    Use generic sorters

--- pipelines/mlrepeat.py
+++ pipelines/mlrepeat.py
@@ -4,46 +4,9 @@ from fsort import Sorter, run, sorters
 
 LOG = logging.getLogger(__name__)
 
-from afirm import B0
-
-class MDixon(Sorter):
+class T1Molli(Sorter):
     def __init__(self):
-        Sorter.__init__(self, "dixon")
-
-    def run(self):
-        self.add(seriesdescription="mdixon", nvols=3)
-        if self.have_files():
-            self.select_latest()
-            self.save("water", vol=0)
-            self.save("fat", vol=1)
-            self.save("fat_fraction", vol=2)
-        self.clear_selection()
-        self.add(seriesdescription="mdixon", nvols=1)
-        if self.have_files():
-            self.select_latest()
-            self.save("t2star", vol=0)
-
-class EThrive(Sorter):
-    def __init__(self):
-        Sorter.__init__(self, "ethrive")
-
-    def run(self):
-        self.add(seriesdescription="thrive")
-        self.select_latest()
-        self.save("ethrive")
-
-class T2w(Sorter):
-    def __init__(self):
-        Sorter.__init__(self, "t2w")
-
-    def run(self):
-        self.add(seriesdescription="t2w")
-        self.select_latest()
-        self.save("t2w")
-
-class MolliKidney(Sorter):
-    def __init__(self):
-        Sorter.__init__(self, "molli_kidney")
+        Sorter.__init__(self, "t1_molli")
 
     def run(self):
         self.add(seriesdescription="kidney_cor_t1_molli", nvols=10)
@@ -58,20 +21,6 @@ class MolliKidney(Sorter):
             self.save("t1_map", vol=0)
             self.save("t1_conf", vol=1)
 
-class AxB1(Sorter):
-    """
-    B1 mapping data
-    """
-    def __init__(self):
-        Sorter.__init__(self, "b1_ax")
-
-    def run_philips(self):
-        self.add(seriesdescription="ax_b1map", imagetype="b1", nvols=1)
-        self.remove(imagetype="phase")
-        if self.have_files():
-            self.select_latest()
-            self.save("b1")
-
 class AxB0(Sorter):
     """
     B0 mapping data
@@ -84,20 +33,6 @@ class AxB0(Sorter):
         if self.have_files():
             self.select_latest()
             self.save("b0")
-
-class CorB1(Sorter):
-    """
-    B1 mapping data
-    """
-    def __init__(self):
-        Sorter.__init__(self, "b1_cor")
-
-    def run_philips(self):
-        self.add(seriesdescription="cor_b1map", imagetype="b1", nvols=1)
-        self.remove(imagetype="phase")
-        if self.have_files():
-            self.select_latest()
-            self.save("b1")
 
 class CorB0(Sorter):
     """
@@ -113,14 +48,14 @@ class CorB0(Sorter):
             self.save("b0")
 
 SORTERS = [
-    MDixon(),
-    EThrive(),
-    T2w(),
-    MolliKidney(),
+    sorters.MDixon(name="dixon", seriesdesc_inc="mdixon"),
+    sorters.SeriesDesc(name="ethrive", seriesdesc=["ethrive", "e_thrive", "thrive"]),
+    sorters.SeriesDesc("t2w"),
+    T1Molli(),
     AxB0(),
+    CorB0(),
     sorters.B1("b1_ax", seriesdescription="ax_b1map", imagetype="b1"),
     sorters.B1("b1_cor", seriesdescription="cor_b1map", imagetype="b1"),
-    CorB0(),
 ]
 
 if __name__ == "__main__":
