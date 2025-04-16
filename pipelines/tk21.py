563f8120ac03483c5347caf6ef8abe099d4c4462
Martin Craig <martin.craig@nottingham.ac.uk>
2025-04-16 15:55:31 +0100

    Add dixon, ethrive, dwi, asl and raw molli / SE

--- pipelines/tk21.py
+++ pipelines/tk21.py
@@ -1,10 +1,10 @@
 """
 FSORT config for travelling_kidney_21 study
 """
-from fsort.sorters import T11MolliMapOrRaw, T2, B1, B0TwoEchos, SeriesDesc, T2star, MTR
+from fsort.sorters import *
+from afirm import DWI, ASL
 
-
-class B0(B0TwoEchos):
+class B0Tk21Special(B0TwoEchos):
     
     def run(self):
         # Special cases of SPMIC subjects with two B0 scans
@@ -13,12 +13,21 @@ class B0(B0TwoEchos):
         B0TwoEchos.run(self)
 
 SORTERS = [
+    T1SERaw(),
+    T1MolliRaw(),
     T11MolliMapOrRaw(),
     T2(),
-    B0(add_removes=["aorta"]),
+    B0Tk21Special(add_removes=["aorta"]),
     B1(seriesdescription="B1"),
     MTR(),
-    SeriesDesc("t1w"),
-    SeriesDesc("t2w"),
+    SeriesDesc("t1w", seriesdesc=["cor_t1w", "t1w"]),
+    SeriesDesc("t2w", seriesdesc=["cor_t2w", "t2w"]),
     T2star(),
+    #MolliAx(),
+    #MolliCor(),
+    MDixon(name="dixon_cor", seriesdesc_inc="cor_mdixon"),
+    MDixon(name="dixon_ax", seriesdesc_inc="mdixon", seriesdesc_exc="cor_mdixon"),
+    SeriesDesc("ethrive"),
+    DWI(),
+    ASL(),
 ]
