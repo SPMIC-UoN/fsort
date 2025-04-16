e97c849ec427a729bc78601609a5d1a25f9d43e9
Martin Craig <martin.craig@nottingham.ac.uk>
2025-04-16 15:55:59 +0100

    Add dwi, asl, dixon, ethrive plus raw molli / SE

--- pipelines/normative.py
+++ pipelines/normative.py
@@ -1,15 +1,25 @@
 """
 FSORT config for normative data study
 """
-from fsort.sorters import T11MolliMapOrRaw, T2, B1, B0TwoEchos, SeriesDesc, T2star, MTR
+from fsort.sorters import *
+from .afirm import DWI, ASL
 
 SORTERS = [
+    T1SERaw(),
+    T1MolliRaw(),
     T11MolliMapOrRaw(),
     T2(),
     B0TwoEchos(add_removes=["aorta"]),
     B1(seriesdescription="B1"),
     MTR(),
-    SeriesDesc("t1w"),
-    SeriesDesc("t2w"),
+    SeriesDesc("t1w", seriesdesc=["cor_t1w", "t1w"]),
+    SeriesDesc("t2w", seriesdesc=["cor_t2w", "t2w"]),
     T2star(),
+    #MolliAx(),
+    #MolliCor(),
+    MDixon(name="dixon_cor", seriesdesc_inc="cor_mdixon"),
+    MDixon(name="dixon_ax", seriesdesc_inc="mdixon", seriesdesc_exc="cor_mdixon"),
+    SeriesDesc("ethrive"),
+    DWI(),
+    ASL(),
 ]
