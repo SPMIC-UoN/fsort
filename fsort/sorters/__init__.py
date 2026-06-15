983982f51af3d3331d58cbf1f16dbf2c26f46786
Martin Craig <martin.craig@nottingham.ac.uk>
2026-06-15 10:01:11 +0100

    Corrected typo in module name

--- fsort/sorters/__init__.py
+++ fsort/sorters/__init__.py
@@ -1,7 +1,7 @@
 from .b1 import B1
 from .b0 import B0TwoEchos
 from .t2 import T2, T2star, ScannerT2Map
-from .t1 import T11MolliMapOrRaw, T1MolliRaw, T1SERaw
+from .t1 import T1MolliMapOrRaw, T1MolliRaw, T1SERaw
 from .generic import SeriesDesc
 from .mtr import MTR
 from .dixon import MDixon, RawDixon
